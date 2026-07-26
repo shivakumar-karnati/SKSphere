0450c990650118f152d23ba0f06a33716ac267e4
Shivakumar <shivakumarkarnati005@gmail.com>
2026-07-26 10:51:56 +0530

    Configure Django settings for production

--- SKSphere/settings.py
+++ SKSphere/settings.py
@@ -22,9 +22,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = os.environ.get(
+    'SECRET_KEY',
+    'django-insecure-local-development-key'
+)
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get(
+    'DEBUG',
+    'True'
+) == 'True'
 
 ALLOWED_HOSTS = [
     'localhost',
